0d9697962e9a499562548198d18fa221036befe5
MeowcaTheoRange <me@abtmtr.link>
2025-04-10 23:37:56 -0500

    Hackify loader, eliminates FOUC on Firefox

--- scripts/furaffinity.net/loader.js
+++ scripts/furaffinity.net/loader.js
@@ -1,29 +1,3 @@
-function createStyleHolder() {
-  let container = document.createElement("div");
-  document.head.appendChild(container);
-  return container;
-}
-function injectStyle(src, parent = document.head) {
-  let url = browser.runtime.getURL("web-accessible/furaffinity.net/styles/" + src);
-  const link = document.createElement("link");
-  link.href = url;
-  link.rel = "stylesheet";
-  parent.appendChild(link);
-}
-async function onHeadLoaded() {
-  const styleHolder = createStyleHolder(document.head);
-  injectStyle("mergeMobileBars.css", styleHolder);
-  injectStyle("mobileWhyYouDoThat.css", styleHolder);
-  injectStyle("noGalleryPreview.css", styleHolder);
-  injectStyle("noMoreNews.css", styleHolder);
-  injectStyle("removeCookiePopup.css", styleHolder);
-  injectStyle("removeFooterLinks.css", styleHolder);
-  injectStyle("removeSiteBanner.css", styleHolder);
-  injectStyle("removeTopbarSupport.css", styleHolder);
-  injectStyle("removeTopbarTransactions.css", styleHolder);
-  injectStyle("showMeTheTags.css", styleHolder);
-}
-
 async function waitFor(selector, base = document, timeout = 1000) {
   const pollInterval = 0;
   const endTime = Date.now() + timeout;
@@ -39,32 +13,75 @@ async function waitFor(selector, base = document, timeout = 1000) {
   });
 }
 
-function createScriptHolder() {
+let styleHolder;
+let scriptHolder;
+
+function createStyleHolder(element) {
   let container = document.createElement("div");
-  document.body.appendChild(container);
+  container.id = "fatweaks-style-holder";
+  element.appendChild(container);
   return container;
 }
+function injectStyle(src, parent = document.head) {
+  let url = browser.runtime.getURL("web-accessible/furaffinity.net/styles/" + src);
+  const link = document.createElement("link");
+  link.href = url;
+  link.rel = "preload stylesheet";
+  link.setAttribute('blocking', 'render');
+  parent.appendChild(link);
+}
+async function onHeadLoaded() {
+  styleHolder = createStyleHolder(document.documentElement);
+  console.log("Loading styles...");
+  injectStyle("mergeMobileBars.css", styleHolder);
+  injectStyle("mobileWhyYouDoThat.css", styleHolder);
+  injectStyle("noBBCodeColor.css", styleHolder);
+  injectStyle("noGalleryPreview.css", styleHolder);
+  injectStyle("noMoreNews.css", styleHolder);
+  injectStyle("removeCookiePopup.css", styleHolder);
+  injectStyle("removeFooterLinks.css", styleHolder);
+  injectStyle("removeSiteBanner.css", styleHolder);
+  injectStyle("removeTopbarSupport.css", styleHolder);
+  injectStyle("removeTopbarTransactions.css", styleHolder);
+  injectStyle("showMeTheTags.css", styleHolder);
+}
+onHeadLoaded();
 
+function createScriptHolder(element) {
+  let container = document.createElement("div");
+  container.id = "fatweaks-script-holder";
+  element.appendChild(container);
+  return container;
+}
 function injectScript(src, parent = document.body) {
   let url = browser.runtime.getURL("web-accessible/furaffinity.net/scripts/" + src);
   const script = document.createElement("script");
   script.src = url;
   script.type = "module";
+  script.defer = true;
   parent.appendChild(script);
 }
-
-function onPageLoaded() {
+async function onPageLoaded() {
   console.log("FurAffiniTweaks loaded!");
-  const scriptHolder = createScriptHolder();
+  scriptHolder = createScriptHolder(document.body);
   injectScript("base.js", scriptHolder);
+
   injectScript("tooltip.js", scriptHolder);
   injectScript("noGalleryPreview.js", scriptHolder);
-  injectScript("nukeAllMessages.js", scriptHolder);
-  injectScript("showMeTheTags.js", scriptHolder);
+
+
   injectScript("titlebarStatus.js", scriptHolder);
   injectScript("liveStatus.js", scriptHolder);
+
+  injectScript("nukeAllMessages.js", scriptHolder);
+  injectScript("showMeTheTags.js", scriptHolder);
+
   injectScript("unwatchDATM.js", scriptHolder);
-  onHeadLoaded();
+  injectScript("systemMessageOverlay.js", scriptHolder);
 }
 
-document.addEventListener("DOMContentLoaded", onPageLoaded);
+document.addEventListener("DOMContentLoaded", () => {
+  onPageLoaded();
+  styleHolder.parentNode.appendChild(styleHolder);
+  scriptHolder.parentNode.appendChild(scriptHolder);
+});
