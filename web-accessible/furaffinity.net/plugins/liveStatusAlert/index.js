d54f7b6cdfeeb9ad62ef88fe02a0c5940bee9194
MeowcaTheoRange <me@abtmtr.link>
2025-04-13 02:46:27 -0500

    Add options to liveStatusAlert

--- web-accessible/furaffinity.net/plugins/liveStatusAlert/index.js
+++ web-accessible/furaffinity.net/plugins/liveStatusAlert/index.js
@@ -1,22 +1,42 @@
-const module = __fatweaks.namespace("tabStatus");
+const module = __fatweaks.namespace("liveStatusAlert");
 
+const settings = __fatweaks.reference("settings");
 const events = __fatweaks.reference("events");
+
+let mySettings = settings.register({
+  name: "Live Status Alert",
+  namespace: "liveStatusAlert"
+});
+let playBlip = mySettings.boolean({
+  id: "playBlip",
+  name: "If a sound should play on a new notification",
+  defaultValue: true
+});
+let flashNumbers = mySettings.boolean({
+  id: "flashNumbers",
+  name: "If the numbers should flash on a new notification",
+  defaultValue: true
+});
 
 const blipURL = __fatweaks.assetURL("liveStatusAlert", "beep.mp3");
 const blip = new Audio(blipURL);
 
 function alert({ notifs, desktop_element, mobile_element }) {
-  try {
-    blip.currentTime = 0;
-    blip.play();
-  } catch (e) { }
+  if (playBlip) {
+    try {
+      blip.currentTime = 0;
+      blip.play();
+    } catch (e) { }
+  }
 
-  desktop_element.classList.add("flash");
-  mobile_element.classList.add("flash");
-  setTimeout(() => {
-    desktop_element.classList.remove("flash");
-    mobile_element.classList.remove("flash");
-  }, 200);
+  if (flashNumbers) {
+    desktop_element.classList.add("flash");
+    mobile_element.classList.add("flash");
+    setTimeout(() => {
+      desktop_element.classList.remove("flash");
+      mobile_element.classList.remove("flash");
+    }, 200);
+  }
 }
 
 events.listenToEvent("liveStatus", "changed", alert);
